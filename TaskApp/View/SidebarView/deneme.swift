0eec7f398e6b42ed1973cf96ff893c89ee6d7c57
Togay Aytemiz <togayaytemiz@gmail.com>
2021-05-10 12:52:44 +0300

    add deneme to be delete

---
+++ TaskApp/View/SidebarView/deneme.swift
@@ -0,0 +1,20 @@
+//
+//  deneme.swift
+//  TaskApp
+//
+//  Created by Togay Aytemiz on 10.05.2021.
+//
+
+import SwiftUI
+
+struct deneme: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct deneme_Previews: PreviewProvider {
+    static var previews: some View {
+        deneme()
+    }
+}
