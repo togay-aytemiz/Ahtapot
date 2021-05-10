e7d8f9e0c59134a0bcc9fa64b743a1ad05cdd7bf
Togay Aytemiz <togayaytemiz@gmail.com>
2021-05-10 13:03:19 +0300

    add deneme3 file

---
+++ TaskApp/View/SidebarView/deneme3.swift
@@ -0,0 +1,20 @@
+//
+//  deneme3.swift
+//  TaskApp
+//
+//  Created by Togay Aytemiz on 10.05.2021.
+//
+
+import SwiftUI
+
+struct deneme3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct deneme3_Previews: PreviewProvider {
+    static var previews: some View {
+        deneme3()
+    }
+}
